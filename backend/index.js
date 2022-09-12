742acc9a1579968885f8844f9de7b20c08914627
Kushagra <kushagrasharma7440@gmail.com>
2022-09-12 13:12:30 +0530

    Creating routes & Schema for storing notes

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,14 @@ connectToMongo();
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello Kushagra!')
-})
+//Avilable Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
+// app.get('/', (req, res) => {
+//   res.send('Hello Kushagra!')
+// })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 })
